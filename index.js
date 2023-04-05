19e1b01d0d2408d31a40d0724d3b446e3894a785
JasperJackalope <125604651+JasperJackalope@users.noreply.github.com>
2023-04-04 20:59:18 -0700

    Fleshed out code, enough for tonight

--- index.js
+++ index.js
@@ -27,38 +27,41 @@ const questions = [
     ]
   },
   {
-      type: 'input',
-      name: 'shapeColor',
-      message: 'Shape Color - please enter the hexadecimal code:',
+   type: 'input',
+   name: 'shapeColor',
+   message: 'Shape Color - please enter the hexadecimal code:',
   }
 ]
 
 inquirer.prompt(questions)
-.then(answers => {;
-    const markdown = generateMarkdown(answers);
+  .then(answers => {
+    const { text, textColor, shape, shapeColor } = answers;
 
-    fs.writeFile('logo.svg', markdown, err => {
-      if (err) {
-        console.error(err);
-        return;
-      }
-      console.log(' Logo generated successfully!');
-    });
+    const svg = SVG().size(300, 200);
+
+    // Draw the shape
+    let shapeElement;
+    switch (shape) {
+      case 'Circle':
+        shapeElement = svg.circle(100).fill(shapeColor).move(50, 50);
+        break;
+      case 'Triangle':
+        shapeElement = svg.polygon('0,100 50,0 100,100').fill(shapeColor).move(50, 50);
+        break;
+      case 'Square':
+        shapeElement = svg.rect(100, 100).fill(shapeColor).move(50, 50);
+        break;
+    }
+
+    // Draw the text
+    const textElement = svg.text(text).fill(textColor).move(100, 75);
+
+    // Get the SVG markup
+    const svgMarkup = svg.svg();
+
+    // Write the SVG markup to a file
+    fs.writeFileSync('logo.svg', svgMarkup);
+
+    console.log('Generated logo.svg');
   })
   .catch(err => console.error(err));
-
-const svg = SVG().size(300, 200);
-
-// Draw the circle
-const circle = svg.circle(100).fill('green').move(50, 50);
-
-// Draw the text
-const text = svg.text('SVG').fill('white').move(100, 75);
-
-// Get the SVG markup
-const svgMarkup = svg.svg();
-
-// Write the SVG markup to a file
-fs.writeFileSync('logo.svg', svgMarkup);
-
-console.log('Generated logo.svg');
