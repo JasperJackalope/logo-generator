1ca3a20f77b5d3d238e5a942ce178783e83327a1
JasperJackalope <125604651+JasperJackalope@users.noreply.github.com>
2023-04-04 20:45:19 -0700

    Added Inqury Questions and Write Code

--- index.js
+++ index.js
@@ -2,21 +2,52 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 const SVG = require('svg.js');
 
-inquirer.prompt([
-    {
-      type: 'input',
-      name: 'text',
-      message: 'Enter up to three characters for the logo:',
-      validate: function(text) {
-        return text.length <= 3;
-      }
+const questions = [
+  {
+    type: 'input',
+    name: 'text',
+    message: 'Please enter up to three characters for the logo:',
+    validate: function(text) {
+      return text.length <= 3;
     }
-  ])
-  .then(function(answers) {
-    // Use the user's response to generate the SVG logo
-  });
+  },
+  {
+    type: 'input',
+    name: 'textColor',
+    message: 'Text Color - please enter the hexadecimal code:',
+  },
+  {
+    type: 'list',
+    name: 'shape',
+    message: 'Please select the shape for the logo:',
+    choices: [
+     'Circle',
+     'Triangle',
+     'Square'
+    ]
+  },
+  {
+      type: 'input',
+      name: 'shapeColor',
+      message: 'Shape Color - please enter the hexadecimal code:',
+  }
+]
 
-  const svg = SVG().size(300, 200);
+inquirer.prompt(questions)
+.then(answers => {;
+    const markdown = generateMarkdown(answers);
+
+    fs.writeFile('logo.svg', markdown, err => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      console.log(' Logo generated successfully!');
+    });
+  })
+  .catch(err => console.error(err));
+
+const svg = SVG().size(300, 200);
 
 // Draw the circle
 const circle = svg.circle(100).fill('green').move(50, 50);
